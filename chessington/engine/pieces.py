9180e33b6b96fec2a3eb2006511e9cde3d3a2544
ianm1968 <57989417+ianm1968@users.noreply.github.com>
2022-05-12 15:59:41 +0100

    Diagonal take tests after cherry pick

--- chessington/engine/pieces.py
+++ chessington/engine/pieces.py
@@ -34,6 +34,9 @@ class Pawn(Piece):
     A class representing a chess pawn.
     """
     def get_available_moves(self, board):
+        
+        moves = []
+        
         current_square = board.find_piece(self)
 
         if self.player == Player.WHITE:
@@ -49,16 +52,23 @@ class Pawn(Piece):
         next_next_square = Square(current_square.row+(direction*2), current_square.col)
         next_square_left = Square(current_square.row+direction, current_square.col-direction)
         next_square_right = Square(current_square.row+direction, current_square.col+direction)
+        
+
             
         if current_square.row == max_row or board.get_piece(next_square) != None:
             return []
         else:
             if current_square.row == start_row and board.get_piece(next_next_square) == None:
-                return [next_square, next_next_square]
+                moves.append(next_square)
+                moves.append(next_next_square)
             else:
-                return [next_square]
-
-
+                moves.append(next_square)
+        left_take = board.get_piece(next_square_left) 
+        if (left_take != None and left_take.player != self.player):
+            moves.append(next_square_left)        
+        if (board.get_piece(next_square_right) != None and board.get_piece(next_square_right).player != self.player):
+            moves.append(next_square_right)
+        return moves
 
 class Knight(Piece):
     """
